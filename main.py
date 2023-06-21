03287c104f256ad08f50850016f5a4e1cdcf8861
csrowley <csrowley@uci.edu>
2023-06-20 22:59:14 -0700

    basic test

--- main.py
+++ main.py
@@ -1,2 +1,28 @@
 import discord
- 
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+token = os.getenv("TOKEN")
+
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'Logged in as {client.user} ({client.user.id})')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+client.run (token)
