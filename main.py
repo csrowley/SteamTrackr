4045e29a6814ed352973b22ab1e378c5d982aac6
csrowley <csrowley@uci.edu>
2023-06-21 16:45:29 -0700

    added test commands

--- main.py
+++ main.py
@@ -1,28 +1,34 @@
 import discord
+from discord.ext import commands
 from dotenv import load_dotenv
 import os
+import random
+
+from steam import Steam
+from decouple import config
+
+KEY = config("STEAM_API_KEY")
+
+steam = Steam(KEY)
+
 
 load_dotenv()
 
 token = os.getenv("TOKEN")
 
-from discord.ext import commands
-
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix='.', intents=intents)
 
-@client.event
+@bot.event
 async def on_ready():
-    print(f'Logged in as {client.user} ({client.user.id})')
+    print(f'Logged in as {bot.user} ({bot.user.id})')
+    print("Registered commands:", bot.commands)
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+@bot.command()
+async def sendnum(ctx):
+    print("Command triggered!")
+    await ctx.reply(f'Your number is: {random.randint(0,999)}')
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
-
-client.run (token)
+bot.run(token)
